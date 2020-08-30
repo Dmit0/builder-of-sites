eec982c607d8010cfa8b223a645c6682d9d9f87a
Dmitrii Shehunov <53045487+Dmit0@users.noreply.github.com>
2020-08-30 12:55:04 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,15 +3,15 @@ import {Site} from './JS/classes/site'
 import { Sidebar } from './JS/classes/sidebar'
 import './css/main.scss'
 
-
-const site=new Site('#site',update)
+const site = new Site('#site')
 
 const update=newBlock=>{
     model.push(newBlock)
     site.render()
 }
 
-new Sidebar('#penel')
+new Sidebar('#panel',update)
 
 site.render(model)
+
 
